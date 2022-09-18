755d33851a9a47d414b59defaa0ea3567b5c702f
Natalia Ivanova <myletters2000@mail.ru>
2022-09-18 20:46:13 +0300

    proba

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ввведите число для проверки:");
+int n = Convert.ToInt32(Console.ReadLine());
+if ((n % 2 == 0))
+       Console.WriteLine ("Четное");
+
+else  Console.WriteLine ("Нечетное");
